69651b9a8db0804e784b9e851864131e0c1ef1ff
Gregory Mundy <gregmundy@gmail.com>
2019-07-22 12:07:37 -0400

    test: add test stub for data trust entity

---
+++ tests/test_data_trust_entity.py
@@ -0,0 +1,20 @@
+"""Data Trust Entity Unit Tests."""
+
+from expects import expect, be
+
+
+class TestDataTrustEntity:
+    def test_create_new_datatrust(self):
+        expect(1).to(be(1))
+
+    def test_edit_datatrust(self):
+        expect(1).to(be(1))
+
+    def test_get_all_datatrusts(self):
+        expect(1).to(be(1))
+
+    def test_delete_datatrust(self):
+        expect(1).to(be(1))
+
+    def test_view_datatrust_details(self):
+        expect(1).to(be(1))
