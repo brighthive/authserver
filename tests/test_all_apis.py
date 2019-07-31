cd4972a2ba1493fc4ec621916f1b9682c6e3c702
Gregory Mundy <gregmundy@gmail.com>
2019-07-30 21:05:07 -0400

    test: add a comprehensive unit test to demonstrate all the apis operating together

---
+++ tests/test_all_apis.py
@@ -0,0 +1,221 @@
+"""Test all API endpoints.
+
+This test class exercises all client facing APIs. It is also usesful as a tool for
+demonstrating how to interact with the various APIs.
+
+"""
+
+import pytest
+import json
+from flask import Response
+from expects import expect, be, equal, raise_error, be_above_or_equal, contain
+from authserver.db import db, DataTrust, User
+
+DATA_TRUST = {
+    'data_trust_name': 'Sample Data Trust'
+}
+
+ROLES = [
+    {
+        'role': 'get:programs',
+        'description': 'Get from programs data resource',
+        'rules': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+            'key4': 'value4'
+        }
+    },
+    {
+        'role': 'administer:programs',
+        'description': 'All access on programs data resource',
+        'rules': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+            'key4': 'value4'
+        }
+    },
+    {
+        'role': 'edit:providers',
+        'description': 'Edit providers only'
+    },
+    {
+        'role': 'view:providers',
+        'description': 'View providers only',
+        'rules': {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key3': 'value3',
+            'key4': 'value4'
+        }
+    }
+]
+
+USERS = [
+    {
+        'firstname': 'Regina',
+        'lastname': 'Wolfgang',
+        'organization': 'BrightHive',
+        'email_address': 'user1@brighthive.me',
+        'username': 'user1',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Kwame',
+        'lastname': 'Anderson',
+        'organization': 'BrightHive',
+        'email_address': 'user2@brighthive.me',
+        'username': 'user2',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Logan',
+        'lastname': 'Williams',
+        'organization': 'BrightHive',
+        'email_address': 'user3@brighthive.me',
+        'username': 'user3',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Amanda',
+        'lastname': 'Stewart',
+        'organization': 'BrightHive',
+        'email_address': 'user4@brighthive.me',
+        'username': 'user4',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Greg',
+        'lastname': 'Henry',
+        'organization': 'BrightHive',
+        'email_address': 'user5@brighthive.me',
+        'username': 'user5',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Tom',
+        'lastname': 'Jones',
+        'organization': 'BrightHive',
+        'email_address': 'user6@brighthive.me',
+        'username': 'user6',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Vyki',
+        'lastname': 'Bennett',
+        'organization': 'BrightHive',
+        'email_address': 'user7@brighthive.me',
+        'username': 'user7',
+        'data_trust_id': ''
+    },
+    {
+        'firstname': 'Danielle',
+        'lastname': 'Bevins',
+        'organization': 'BrightHive',
+        'email_address': 'user8@brighthive.me',
+        'username': 'user8',
+        'data_trust_id': ''
+    }
+]
+
+CLIENTS = [
+    {
+        'client_name': 'test client 1',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 2',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 3',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 4',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 5',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client6',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 7',
+        'user_id': ''
+    },
+    {
+        'client_name': 'test client 8',
+        'user_id': ''
+    }
+]
+
+
+class TestAllAPIs(object):
+    def test_all_apis(self, client):
+        # Common headers go in this dict
+        headers = {'content-type': 'application/json'}
+
+        # Create an overarching data trust
+        response: Response = client.post('/data_trusts', data=json.dumps(DATA_TRUST), headers=headers)
+        expect(response.status_code).to(equal(201))
+
+        data_trust_id = response.json['response'][0]['id']
+
+        # Create users
+        user_ids = []
+        for user in USERS:
+            user['data_trust_id'] = data_trust_id
+            response = client.post('/users', data=json.dumps(user), headers=headers)
+            expect(response.status_code).to(equal(201))
+            user_ids.append(response.json['response'][0]['id'])
+
+        # Create roles
+        role_ids = []
+        for role in ROLES:
+            response = client.post('/roles', data=json.dumps(role), headers=headers)
+            expect(response.status_code).to(equal(201))
+            role_ids.append(response.json['response'][0]['id'])
+
+        expect(len(user_ids)).to(equal(8))
+
+        # Create clients
+        client_ids = []
+        for i, api_client in enumerate(CLIENTS):
+            api_client['user_id'] = user_ids[i]
+            response = client.post('/clients', data=json.dumps(api_client))
+            expect(response.status_code).to(equal(201))
+            client_ids.append(response.json['response'][0]['id'])
+
+        # Assign clients to users and roles to client
+        for i, client_id in enumerate(client_ids):
+            request_body = {
+                'user_id': user_ids[i],
+                'roles': role_ids
+            }
+            response = client.patch('/clients/{}'.format(client_id), data=json.dumps(request_body), headers=headers)
+            expect(response.status_code).to(equal(200))
+
+        # Ensure that clients actually have roles and users
+        for client_id in client_ids:
+            response = client.get('/clients/{}'.format(client_id), headers=headers)
+            result = response.json['response']
+            expect(result['id']).to(equal(client_id))
+            expect(user_ids).to(contain(result['user_id']))
+            expect(len(result['roles'])).to(equal(len(role_ids)))
+
+        # Data Cleanup
+        for role_id in role_ids:
+            response = client.delete('/roles/{}'.format(role_id), headers=headers)
+            expect(response.status_code).to(equal(200))
+
+        for user_id in user_ids:
+            response = client.delete('/users/{}'.format(user_id), headers=headers)
+            expect(response.status_code).to(equal(200))
+
+        response = client.delete('/data_trusts/{}'.format(data_trust_id), headers=headers)
+        expect(response.status_code).to(equal(200))
