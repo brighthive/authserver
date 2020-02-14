0b5c3e524377e406d9a2111decefd15e811bf062
Greg Mundy <gregmundy@gmail.com>
2020-02-13 20:29:01 -0500

    feat: add users to seed data
    Add a user to the seed data for migration.

--- migrations/versions/1794e645d265_.py
+++ migrations/versions/1794e645d265_.py
@@ -8,6 +8,7 @@ Create Date: 2020-02-12 17:37:49.538592
 
 import os
 import sqlalchemy as sa
+import bcrypt
 from sqlalchemy.sql import table, column
 from alembic import op
 from datetime import datetime
@@ -27,15 +28,52 @@ data_trust_table = table('data_trusts',
                          column('date_created', sa.TIMESTAMP),
                          column('date_last_updated', sa.TIMESTAMP))
 
+user_table = table('users',
+                   column('id', sa.String),
+                   column('username', sa.String),
+                   column('firstname', sa.String),
+                   column('lastname', sa.String),
+                   column('organization', sa.String),
+                   column('email_address', sa.String),
+                   column('active', sa.Boolean),
+                   column('data_trust_id', sa.String, sa.ForeignKey('data_trusts.id', ondelete='CASCADE')),
+                   column('password_hash', sa.String),
+                   column('date_created', sa.TIMESTAMP),
+                   column('date_last_updated', sa.TIMESTAMP))
+
+
+def hash_password(password: str):
+    return bcrypt.hashpw(password.encode(
+        'utf-8'), bcrypt.gensalt()).decode('utf-8')
+
 
 def upgrade():
     environment = os.getenv('APP_ENV', None)
     data_trust_id = str(uuid4()).replace('-', '')
+    admin_user_id = str(uuid4()).replace('-', '')
     if environment == 'TESTING':
+        # Set up data trust
         op.bulk_insert(data_trust_table, [
             {
                 'id': data_trust_id,
                 'data_trust_name': 'Test Data Trust',
+                'date_created': datetime.utcnow(),
+                'date_last_updated': datetime.utcnow()
+            }
+        ])
+
+        # Set up test user
+        op.bulk_insert(user_table, [
+            {
+                'id': admin_user_id,
+                'username': 'test_user',
+                'firstname': 'Test',
+                'lastname': 'User',
+                'organization': 'BrightHive',
+                'email_address': 'test_user@brighthive.net',
+                'active': True,
+                'data_trust_id': data_trust_id,
+                'password_hash': hash_password('passw0rd'),
                 'date_created': datetime.utcnow(),
                 'date_last_updated': datetime.utcnow()
             }
@@ -45,6 +83,22 @@ def upgrade():
             {
                 'id': data_trust_id,
                 'data_trust_name': 'Sample Data Trust',
+                'date_created': datetime.utcnow(),
+                'date_last_updated': datetime.utcnow()
+            }
+        ])
+
+        op.bulk_insert(user_table, [
+            {
+                'id': admin_user_id,
+                'username': 'facet_admin',
+                'firstname': 'Facet',
+                'lastname': 'Administrator',
+                'organization': 'BrightHive',
+                'email_address': 'brighthive_admin@brighthive.net',
+                'active': True,
+                'data_trust_id': data_trust_id,
+                'password_hash': hash_password('143DATATRUST341'),
                 'date_created': datetime.utcnow(),
                 'date_last_updated': datetime.utcnow()
             }
