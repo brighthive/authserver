151c90befc28ef777b616efb826df3d1927dfd8a
Regina Compton <reginafcompton@gmail.com>
2020-11-19 15:49:43 -0500

    task: Add coniditonal for can_login and write tests

--- tests/api/test_login.py
+++ tests/api/test_login.py
@@ -1,14 +1,65 @@
 import json
-from flask import Response, session
-
-import pytest
 from unittest.mock import patch
 from urllib.parse import urlencode
-from expects import expect, equal, be_empty
+
+import pytest
+from expects import be_empty, equal, expect
+from flask import Response, session
+
+from authserver.db import User, db
 
 
-@pytest.mark.skip(reason=None)
+USER_DATA = {
+    "username": "greg_henry",
+    "password": "passw0rd!",
+    "role_id": None,
+    "person_id": None,
+    "can_login": True,
+    "active": True
+}
+
+
 class TestLogin:
+    def test_valid_user_login_with_redirect(self, client):
+        user = User(**USER_DATA)
+        db.session.add(user)
+
+        response = client.post(
+            '/', data={"username": "greg_henry", "password": "passw0rd!"})
+
+        assert "<title>Redirecting...</title>" in str(response.data)
+
+    def test_invalid_username_password(self, client):
+        response = client.post(
+            '/', data={"username": "not_a_username", "password": "invalid_password"})
+
+        assert "You did not enter valid login credentials." in str(
+            response.data)
+
+    def test_user_can_login_as_false(self, client):
+        USER_DATA["can_login"] = False
+        user = User(**USER_DATA)
+        db.session.add(user)
+
+        response = client.post(
+            '/', data={"username": "greg_henry", "password": "passw0rd!"})
+
+        assert "You did not enter valid login credentials." in str(
+            response.data)
+
+    def test_user_active_as_false(self, client):
+        USER_DATA["can_login"] = True
+        USER_DATA["active"] = False
+        user = User(**USER_DATA)
+        db.session.add(user)
+
+        response = client.post(
+            '/', data={"username": "greg_henry", "password": "passw0rd!"})
+
+        assert "You did not enter valid login credentials." in str(
+            response.data)
+
+    @pytest.mark.skip(reason=None)
     def test_session_clear_after_consent(self, client, user):
         with client:
             # `session_transaction` provides a way to simulate a call to the current SecureCookieSession, allowing us to check or modify its value (before and after making an HTTP request)
