1bd8b05fdcb5b758288c971ee1f6226e963c563d
Greg Mundy <gregmundy@gmail.com>
2021-02-02 23:09:51 -0500

    feat: add reset password form
    This form adds all the required "screens" to support the entire password reset epic. Next step is to add the application logic to enable the complete workflow.

--- authserver/api/home.py
+++ authserver/api/home.py
@@ -5,6 +5,8 @@ import random
 from flask import Blueprint, render_template, request, redirect, url_for, session, g
 from wtforms import Form, StringField, PasswordField, validators
 
+from authserver.config import AbstractConfiguration
+
 # TODO: Move into code after testing
 import sendgrid
 from sendgrid.helpers.mail import Email, To, Content, Mail, template_id
@@ -29,6 +31,11 @@ class LoginForm(Form):
 class RecoverPasswordForm(Form):
     username = StringField(
         'Username', [validators.DataRequired(), validators.length(min=4, max=40)])
+
+
+class ResetPasswordForm(Form):
+    password = PasswordField('Enter your new password', [validators.DataRequired()])
+    confirm_password = PasswordField('Re-type your new password', [validators.DataRequired()])
 
 
 @home_bp.route('/', methods=['GET', 'POST'])
@@ -64,7 +71,7 @@ def login():
 
 
 @home_bp.route('/password-recovery', methods=['GET', 'POST'])
-def recover_password():
+def recover_password(config: AbstractConfiguration):
     errors = None
     form = RecoverPasswordForm(request.form)
     if request.method == 'GET':
@@ -81,4 +88,25 @@ def recover_password():
         # print(response.body)
         # print(response.headers)
         print(request.url_root + 'reset-password?q=' + string_num_generator(30))
-        return render_template('success.html')
+        return render_template('success.html', default_app_url=config.default_app_url, heading='Email Sent!', body='A password reset email has been sent to the email address associated with the entered username.')
+
+
+@home_bp.route('/password-reset', methods=['GET', 'POST'])
+def reset_password(config: AbstractConfiguration):
+    errors = None
+    form = ResetPasswordForm(request.form)
+    if request.method == 'GET':
+        return render_template('reset.html', form=form)
+    else:
+        # sg = sendgrid.SendGridAPIClient(api_key='foo')
+        # from_email = Email('foo')
+        # to_emails = [To('foo')]
+        # template_id = 'foo'
+        # mail = Mail(from_email, to_emails)
+        # mail.template_id = template_id
+        # # response = sg.client.mail.send.post(request_body=mail.get())
+        # # print(response.status_code)
+        # # print(response.body)
+        # # print(response.headers)
+        # print(request.url_root + 'reset-password?q=' + string_num_generator(30))
+        return render_template('success.html', default_app_url=config.default_app_url, heading='Update Successful!', body='Please log in with your new credentials.')
