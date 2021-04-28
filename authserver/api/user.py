47b5360892cc01c11f11b6a81084c825f7ccdb24
Regina Compton <reginafcompton@gmail.com>
2021-04-28 12:48:34 -0400

    fix: Expire ALL user tokens

--- authserver/api/user.py
+++ authserver/api/user.py
@@ -197,8 +197,8 @@ class UserResource(Resource):
         user.can_login = False
         user.date_last_updated = datetime.utcnow()
 
-        token = OAuth2Token.query.filter_by(user_id=user.id).first()
-        if token:
+        tokens = OAuth2Token.query.filter_by(user_id=user.id).all()
+        for token in tokens:
             token.revoked = True
             token.expires_in = 0
 
