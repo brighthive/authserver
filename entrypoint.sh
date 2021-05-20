a0cb619fa2c2541bb6f55bcdc5c13aa85d864e52
Logan <logan@brighthive.io>
2021-05-19 19:32:33 -0700

    revert change

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ done
 
 if [ "$APP_ENV" == "DEVELOPMENT" ] || [ -z "$APP_ENV" ]; then
     export AUTHLIB_INSECURE_TRANSPORT=1
-    gunicorn -w 4 -b 0.0.0.0:10001 wsgi:app --reload --worker-class gevent --timeout 600
+    gunicorn -w 4 -b 0.0.0.0:8000 wsgi:app --reload --worker-class gevent --timeout 600
 else
     gunicorn -b 0.0.0.0 -w $WORKERS wsgi:app --worker-class gevent
 fi
